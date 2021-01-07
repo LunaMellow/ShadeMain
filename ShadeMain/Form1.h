371ad6ce0defb8cb7f286bab1346b63dc0593748
Sofia-pastrami <nore0404@gmail.com>
2021-01-07 18:07:08 +0100

    Added user and pass input + logo in UI

--- ShadeMain/Form1.h
+++ ShadeMain/Form1.h
@@ -36,8 +36,18 @@ namespace CppCLRWinformsProjekt {
 		}
 	private: Bunifu::Framework::UI::BunifuElipse^ FormElipse;
 	private: System::Windows::Forms::Panel^ LeftPanel;
-	private: Bunifu::Framework::UI::BunifuTileButton^ bunifuTileButton1;
-	private: Bunifu::Framework::UI::BunifuThinButton2^ ExitButton;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ PassInput;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ UserInput;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label1;
+
+
+
+
+
+
 	protected:
 
 	protected:
@@ -57,11 +67,14 @@ namespace CppCLRWinformsProjekt {
 		void InitializeComponent(void)
 		{
 			this->components = (gcnew System::ComponentModel::Container());
-			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Form1::typeid));
 			this->FormElipse = (gcnew Bunifu::Framework::UI::BunifuElipse(this->components));
-			this->ExitButton = (gcnew Bunifu::Framework::UI::BunifuThinButton2());
 			this->LeftPanel = (gcnew System::Windows::Forms::Panel());
-			this->bunifuTileButton1 = (gcnew Bunifu::Framework::UI::BunifuTileButton());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->UserInput = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->PassInput = (gcnew System::Windows::Forms::Label());
 			this->LeftPanel->SuspendLayout();
 			this->SuspendLayout();
 			// 
@@ -70,69 +83,84 @@ namespace CppCLRWinformsProjekt {
 			this->FormElipse->ElipseRadius = 10;
 			this->FormElipse->TargetControl = this;
 			// 
-			// ExitButton
-			// 
-			this->ExitButton->ActiveBorderThickness = 1;
-			this->ExitButton->ActiveCornerRadius = 20;
-			this->ExitButton->ActiveFillColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(205)),
-				static_cast<System::Int32>(static_cast<System::Byte>(180)), static_cast<System::Int32>(static_cast<System::Byte>(219)));
-			this->ExitButton->ActiveForecolor = System::Drawing::Color::White;
-			this->ExitButton->ActiveLineColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(205)),
-				static_cast<System::Int32>(static_cast<System::Byte>(180)), static_cast<System::Int32>(static_cast<System::Byte>(219)));
-			this->ExitButton->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
-			this->ExitButton->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(205)), static_cast<System::Int32>(static_cast<System::Byte>(180)),
-				static_cast<System::Int32>(static_cast<System::Byte>(219)));
-			this->ExitButton->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"ExitButton.BackgroundImage")));
-			this->ExitButton->ButtonText = L"x";
-			this->ExitButton->Cursor = System::Windows::Forms::Cursors::Hand;
-			this->ExitButton->Font = (gcnew System::Drawing::Font(L"Century Gothic", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->ExitButton->ForeColor = System::Drawing::Color::Transparent;
-			this->ExitButton->IdleBorderThickness = 1;
-			this->ExitButton->IdleCornerRadius = 20;
-			this->ExitButton->IdleFillColor = System::Drawing::Color::White;
-			this->ExitButton->IdleForecolor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(109)), static_cast<System::Int32>(static_cast<System::Byte>(89)),
-				static_cast<System::Int32>(static_cast<System::Byte>(122)));
-			this->ExitButton->IdleLineColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(109)), static_cast<System::Int32>(static_cast<System::Byte>(89)),
-				static_cast<System::Int32>(static_cast<System::Byte>(122)));
-			this->ExitButton->Location = System::Drawing::Point(923, 8);
-			this->ExitButton->Margin = System::Windows::Forms::Padding(5);
-			this->ExitButton->Name = L"ExitButton";
-			this->ExitButton->Size = System::Drawing::Size(25, 34);
-			this->ExitButton->TabIndex = 0;
-			this->ExitButton->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
-			this->ExitButton->Click += gcnew System::EventHandler(this, &Form1::ExitButton_Click);
-			// 
 			// LeftPanel
 			// 
 			this->LeftPanel->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(200)),
 				static_cast<System::Int32>(static_cast<System::Byte>(221)));
-			this->LeftPanel->Controls->Add(this->bunifuTileButton1);
+			this->LeftPanel->Controls->Add(this->PassInput);
+			this->LeftPanel->Controls->Add(this->label2);
+			this->LeftPanel->Controls->Add(this->UserInput);
+			this->LeftPanel->Controls->Add(this->textBox1);
+			this->LeftPanel->Controls->Add(this->label1);
 			this->LeftPanel->Dock = System::Windows::Forms::DockStyle::Left;
 			this->LeftPanel->Location = System::Drawing::Point(0, 0);
 			this->LeftPanel->Name = L"LeftPanel";
 			this->LeftPanel->Size = System::Drawing::Size(200, 561);
 			this->LeftPanel->TabIndex = 1;
 			// 
-			// bunifuTileButton1
+			// button1
 			// 
-			this->bunifuTileButton1->BackColor = System::Drawing::Color::Transparent;
-			this->bunifuTileButton1->color = System::Drawing::Color::SeaGreen;
-			this->bunifuTileButton1->colorActive = System::Drawing::Color::MediumSeaGreen;
-			this->bunifuTileButton1->Cursor = System::Windows::Forms::Cursors::Hand;
-			this->bunifuTileButton1->Font = (gcnew System::Drawing::Font(L"Century Gothic", 15.75F));
-			this->bunifuTileButton1->ForeColor = System::Drawing::Color::White;
-			this->bunifuTileButton1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"bunifuTileButton1.Image")));
-			this->bunifuTileButton1->ImagePosition = 20;
-			this->bunifuTileButton1->ImageZoom = 50;
-			this->bunifuTileButton1->LabelPosition = 41;
-			this->bunifuTileButton1->LabelText = L"Anonymous";
-			this->bunifuTileButton1->Location = System::Drawing::Point(15, 8);
-			this->bunifuTileButton1->Margin = System::Windows::Forms::Padding(6);
-			this->bunifuTileButton1->Name = L"bunifuTileButton1";
-			this->bunifuTileButton1->Size = System::Drawing::Size(170, 129);
-			this->bunifuTileButton1->TabIndex = 0;
-			this->bunifuTileButton1->Click += gcnew System::EventHandler(this, &Form1::bunifuTileButton1_Click);
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->ForeColor = System::Drawing::Color::White;
+			this->button1->Location = System::Drawing::Point(914, 13);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(35, 23);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"Exit";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Rage Italic", 50));
+			this->label1->ForeColor = System::Drawing::Color::White;
+			this->label1->Location = System::Drawing::Point(6, 478);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(187, 84);
+			this->label1->TabIndex = 3;
+			this->label1->Text = L"Shade";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(20, 137);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(162, 20);
+			this->textBox1->TabIndex = 4;
+			// 
+			// UserInput
+			// 
+			this->UserInput->Location = System::Drawing::Point(18, 93);
+			this->UserInput->Name = L"UserInput";
+			this->UserInput->Size = System::Drawing::Size(162, 20);
+			this->UserInput->TabIndex = 5;
+			this->UserInput->TextChanged += gcnew System::EventHandler(this, &Form1::UserInput_TextChanged);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(41)), static_cast<System::Int32>(static_cast<System::Byte>(50)),
+				static_cast<System::Int32>(static_cast<System::Byte>(65)));
+			this->label2->Location = System::Drawing::Point(16, 69);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(70, 20);
+			this->label2->TabIndex = 6;
+			this->label2->Text = L"Username";
+			// 
+			// PassInput
+			// 
+			this->PassInput->AutoSize = true;
+			this->PassInput->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->PassInput->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(41)), static_cast<System::Int32>(static_cast<System::Byte>(50)),
+				static_cast<System::Int32>(static_cast<System::Byte>(65)));
+			this->PassInput->Location = System::Drawing::Point(18, 114);
+			this->PassInput->Name = L"PassInput";
+			this->PassInput->Size = System::Drawing::Size(68, 20);
+			this->PassInput->TabIndex = 7;
+			this->PassInput->Text = L"Password";
 			// 
 			// Form1
 			// 
@@ -140,23 +168,26 @@ namespace CppCLRWinformsProjekt {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(205)), static_cast<System::Int32>(static_cast<System::Byte>(180)),
 				static_cast<System::Int32>(static_cast<System::Byte>(219)));
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
 			this->ClientSize = System::Drawing::Size(964, 561);
+			this->Controls->Add(this->button1);
 			this->Controls->Add(this->LeftPanel);
-			this->Controls->Add(this->ExitButton);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Name = L"Form1";
 			this->Text = L"Shade";
 			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
 			this->LeftPanel->ResumeLayout(false);
+			this->LeftPanel->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
 	private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
-	private: System::Void bunifuTileButton1_Click(System::Object^ sender, System::EventArgs^ e) {
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->Close();
 	}
-private: System::Void ExitButton_Click(System::Object^ sender, System::EventArgs^ e) {
+private: System::Void UserInput_TextChanged(System::Object^ sender, System::EventArgs^ e) {
 	
 }
 };
